aa6990fc79c472dfe0b4e447e09c005a5bcc1f24
Russ <45741898+Vulp35@users.noreply.github.com>
2022-12-03 07:41:58 -0600

    Final refactor?

--- src/main/java/net/vulp35/worldnamegen/WorldNameGen.java
+++ src/main/java/net/vulp35/worldnamegen/WorldNameGen.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 
 public class WorldNameGen implements ClientModInitializer {
 
-	public static final String MOD_ID = "modid";
+	public static final String MOD_ID = "worldnamegen";
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
