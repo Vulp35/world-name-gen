63caa2f3a22fbf259d88abb5d7b06b4e591c31d1
Russ <45741898+Vulp35@users.noreply.github.com>
2022-12-10 19:30:44 -0600

    cleanup ConfigGui

--- src/main/java/net/vulp35/worldnamegen/config/ConfigGui.java
+++ src/main/java/net/vulp35/worldnamegen/config/ConfigGui.java
@@ -5,22 +5,29 @@ import dev.isxander.yacl.api.Option;
 import dev.isxander.yacl.api.YetAnotherConfigLib;
 import dev.isxander.yacl.gui.controllers.*;
 import dev.isxander.yacl.gui.controllers.slider.IntegerSliderController;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 import net.vulp35.worldnamegen.WorldNameGen;
 
 public class ConfigGui {
 
-    public static void save() {
-        MinecraftClient.getInstance().options.write();
-        WorldNameGen.getConfig().save();
-    }
     public static Screen createGui(Screen parent) {
         return YetAnotherConfigLib.create(WorldNameGen.getConfig(), (defaults, config, builder) -> builder
                 .title(Text.of("World Name Generator"))
                 .category(ConfigCategory.createBuilder()
                         .name(Text.of("World Name Generator"))
+                        .option(Option.createBuilder(int.class)
+                                .name(Text.of("Max Length of Generated Name"))
+                                .tooltip(Text.of("Sets the longest the generated name can be.\n" +
+                                        "This may limit how many unique names are generated."))
+                                .binding(
+                                        defaults.maxNameLength,
+                                        () -> config.maxNameLength,
+                                        (value) -> config.maxNameLength = value
+                                )
+                                .controller(opt -> new IntegerSliderController(opt, 7, 32, 1))
+                                .build()
+                        )
                         .option(Option.createBuilder(boolean.class)
                                 .name(Text.of("Enable Seed name generation"))
                                 .tooltip(Text.of("This option puts the generate button on the seed screen"))
@@ -32,21 +39,9 @@ public class ConfigGui {
                                 .available(false)
                                 .controller(BooleanController::new)
                                 .build()
-                        )
-                        .option(Option.createBuilder(int.class)
-                                .name(Text.of("Max Length of Generated Name"))
-                                .tooltip(Text.of("Sets the longest the generated name can be.\n" +
-                                                        "This may limit how many unique names are generated."))
-                                .binding(
-                                        defaults.maxNameLength,
-                                        () -> config.maxNameLength,
-                                        (value) -> config.maxNameLength = value
-                                )
-                                .controller(opt -> new IntegerSliderController(opt, 7, 32, 1))
-                                .build()
+
                         )
                         .build())
-                                .save(ConfigGui::save)
                 )
                 .generateScreen(parent);
     }
