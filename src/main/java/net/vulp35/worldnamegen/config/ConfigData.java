68c0be0e584050484fc4dc5cd4b863f3b0551106
Russ <45741898+Vulp35@users.noreply.github.com>
2022-12-14 13:21:27 -0600

    buttons disappear when More Options button is pressed. Added seed button and linked visibility to config.

--- src/main/java/net/vulp35/worldnamegen/config/ConfigData.java
+++ src/main/java/net/vulp35/worldnamegen/config/ConfigData.java
@@ -3,7 +3,8 @@ package net.vulp35.worldnamegen.config;
 import dev.isxander.yacl.config.ConfigEntry;
 
 public class ConfigData {
-    @ConfigEntry public boolean buttonOnSeedScreen = false;
+
     @ConfigEntry public int maxNameLength = 32;
+    @ConfigEntry public boolean buttonOnSeedScreen = false;
 
 }
--- src/main/java/net/vulp35/worldnamegen/mixin/CreateWorldScreenMixin.java
+++ src/main/java/net/vulp35/worldnamegen/mixin/CreateWorldScreenMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.gui.widget.TexturedButtonWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.vulp35.worldnamegen.WorldNameGen;
 import net.vulp35.worldnamegen.config.ConfigGui;
 import net.vulp35.worldnamegen.utils.NameGenerator;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,26 +23,57 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class CreateWorldScreenMixin extends Screen {
 
     @Shadow private TextFieldWidget levelNameField;
+    @Shadow private boolean moreOptionsOpen;
+
+    private ButtonWidget generateNameButton;
+    private ButtonWidget settingsButton;
+    private ButtonWidget generateSeedButton;
+
     private static final Identifier GENERATE_BUTTON_TEXTURE = new Identifier("worldnamegen", "textures/gui/generate.png");
     private static final Identifier SETTINGS_BUTTON_TEXTURE = new Identifier("worldnamegen", "textures/gui/settings.png");
 
     @Shadow protected abstract <T extends Element & Drawable & Selectable> T addDrawableChild(T drawableElement);
 
-    protected CreateWorldScreenMixin(Text title, TextFieldWidget levelNameField) {
+    protected CreateWorldScreenMixin(Text title, TextFieldWidget levelNameField, boolean moreOptionsOpen) {
         super(title);
         this.levelNameField = levelNameField;
+        this.moreOptionsOpen = moreOptionsOpen;
     }
 
-    @Inject(at = @At("RETURN"), method = "init")
+    @Inject(at = @At("RETURN"), method = "init()V")
     private void AddWorldNameGenButtons(CallbackInfo ci) {
-        ButtonWidget generateNameButton = this.addDrawableChild(
+        this.generateNameButton = this.addDrawableChild(
                 new TexturedButtonWidget(this.width / 2 + 105, 60, 20, 20, 0, 0, 20,
                         GENERATE_BUTTON_TEXTURE, 32, 64,
                         (button) -> this.levelNameField.setText(NameGenerator.getNewName())));
-        ButtonWidget settingsButton = this.addDrawableChild(
+        this.settingsButton = this.addDrawableChild(
                 new TexturedButtonWidget(this.width / 2 + 130, 60, 20, 20, 0, 0, 20,
                         SETTINGS_BUTTON_TEXTURE, 32, 64,
                         (button) -> this.client.setScreen(ConfigGui.createGui(client.currentScreen))));
+
+        this.generateSeedButton = this.addDrawableChild(
+                new TexturedButtonWidget(this.width / 2 + 105, 60, 20, 20, 0, 0, 20,
+                        GENERATE_BUTTON_TEXTURE, 32, 64,
+                        (button) -> {
+
+                        } ));
+    }
+
+    @Inject(at = @At("RETURN"), method = "setMoreOptionsOpen*")
+    private void moreOptionsScreenOpen(CallbackInfo ci) {
+        if (this.generateNameButton != null) {
+            this.generateNameButton.visible = !moreOptionsOpen;
+        }
+        if (this.settingsButton != null) {
+            this.settingsButton.visible = !moreOptionsOpen;
+        }
+        if (this.generateSeedButton != null ) {
+            if (WorldNameGen.getConfig().getConfig().buttonOnSeedScreen) {
+                this.generateSeedButton.visible = moreOptionsOpen;
+            } else {
+                this.generateSeedButton.visible = false;
+            }
+        }
     }
 
 }
